067f16f8e5aeb69da0c76af881887691a1b65e45
Ikhsan Fauzan <aingikhsan15@gmail.com>
2026-02-09 10:39:05 +0700

    Modul PHP KL11

--- data/Car.php
+++ data/Car.php
@@ -1,11 +1,27 @@
 <?php
 
+interface HusBrand {
+    function getBrand (): string;
+}
+
+interface isMaintenance {
+    function isMaintenance(): bool;
+}
+
 interface Car {
     function drive(): void;
     function getTire(): int;
 }
 
-class avanza implements Car {
+class avanza implements Car, isMaintenance, HusBrand {
+    function getBrand(): string {
+        return "Toyota";
+    }
+
+    function isMaintenance(): bool {
+        return false;
+    }
+
     function drive(): void {
         echo "Drive AVANZA" . PHP_EOL;
     }
--- interface.php
+++ interface.php
@@ -3,5 +3,7 @@
 require_once 'data/Car.php';
 
 $avanza = new Avanza();
+$avanza -> getBrand();
 $avanza -> drive();
 echo $avanza -> getTire();
+echo $avanza -> isMaintenance();
